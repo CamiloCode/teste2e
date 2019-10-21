033b2083e55bc5d0aacbfda0ac4c51857963c024
Camilo Contreras <omar.contreras@whitehack.co>
2019-10-21 01:40:57 -0500

    set latest cucumber deps

--- src/test/java/com/test/selenium/Runner.java
+++ src/test/java/com/test/selenium/Runner.java
@@ -1,8 +1,10 @@
 package com.test.selenium;
 
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
 public class Runner {
 }
